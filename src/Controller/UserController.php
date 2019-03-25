037538c5e07495b91fd86b8804abf7e12722f255
dayaaan <dayaaan.vu@gmail.com>
2019-03-25 13:01:17 +0100

    service navigation

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -14,8 +14,8 @@ class UserController extends AbstractController
     {
         
         return $this->render('user/index.html.twig', [
-            'user' => $user,
             'navigation' => $navigation,
+            'user' => $user,
         ]);
     }
 }
--- src/Service/Navigation.php
+++ src/Service/Navigation.php
@@ -5,17 +5,20 @@ namespace App\Service;
 use Twig\Environment;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class Navigation {
 
     private $request;
     private $logger;
     private $templatePath;
+    private $tokenStorage;
 
-    public function __construct(RequestStack $request, Environment $twig, $templatePath) {
+    public function __construct(RequestStack $request, Environment $twig, $templatePath, TokenStorageInterface $tokenStorage) {
         $this->request = $request;
         $this->twig = $twig;
         $this->templatePath = $templatePath;
+        $this->tokenStorage = $tokenStorage;
     }
 
     public function setTemplatePath($templatePath) {
@@ -26,11 +29,11 @@ class Navigation {
     public function getTemplatePath() {
         return $this->templatePath;
     }
-
+    // Récup le chemin courant
     public function getCurrentPath() {
         return $this->request->getCurrentRequest()->attributes->get('_route');
     }
-
+    // Recup le chemin courant en lui appliquant des transformations
     public function getPath() {
         return strtoupper(str_replace("_", " ", $this->getCurrentPath()));
     }
@@ -39,6 +42,8 @@ class Navigation {
         $this->twig->display($this->templatePath, [
             'currentPath' => $this->getCurrentPath(),
             'path' => $this->getPath(),
+            // Récuperer l'utilisateur courant
+            'user' => $this->tokenStorage->getToken()->getUser(),
         ]);
     }
 
